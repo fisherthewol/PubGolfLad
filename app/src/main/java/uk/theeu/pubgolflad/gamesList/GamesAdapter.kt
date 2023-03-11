d90fe89c248101144e811c88e8a8f56ad6ecb001
George Kokinis <george.kokinis@gmail.com>
2023-03-11 23:24:30 +0000

    Actually add layout inflation.

--- app/src/main/java/uk/theeu/pubgolflad/gamesList/GamesAdapter.kt
+++ app/src/main/java/uk/theeu/pubgolflad/gamesList/GamesAdapter.kt
@@ -1,12 +1,15 @@
 package uk.theeu.pubgolflad.gamesList
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import uk.theeu.pubgolflad.R
 
 class GamesAdapter: RecyclerView.Adapter<GamesAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GamesAdapter.ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.games_row_item, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: GamesAdapter.ViewHolder, position: Int) {

058df13f5090868848dbe9a036c1212726822a49
George Kokinis <george.kokinis@gmail.com>
2023-03-11 23:03:18 +0000

    Sketch GamesAdapter

---
+++ app/src/main/java/uk/theeu/pubgolflad/gamesList/GamesAdapter.kt
@@ -0,0 +1,23 @@
+package uk.theeu.pubgolflad.gamesList
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class GamesAdapter: RecyclerView.Adapter<GamesAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GamesAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: GamesAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    }
+}

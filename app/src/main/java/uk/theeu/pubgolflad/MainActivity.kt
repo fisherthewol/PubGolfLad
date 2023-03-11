c471bf77523dff655bcc78b668aa42b3e648902e
George Kokinis <george.kokinis@gmail.com>
2023-03-11 20:07:54 +0000

    Change id; add log to handle settings.

--- app/src/main/java/uk/theeu/pubgolflad/MainActivity.kt
+++ app/src/main/java/uk/theeu/pubgolflad/MainActivity.kt
@@ -2,11 +2,28 @@ package uk.theeu.pubgolflad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.topAppBar))
+        setSupportActionBar(findViewById(R.id.top_app_bar))
+        (findViewById<MaterialToolbar>(R.id.top_app_bar)).setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.top_bar_settings -> {
+                    Log.d("top_app_bar","Settings Pressed")
+                    true
+                }
+                else -> {false}
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_page_top_app_bar, menu)
+        return true
     }
 }

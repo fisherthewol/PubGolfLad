735566dc487649a3f1834bf51addbdf01335cc40
George Kokinis <george.kokinis@gmail.com>
2023-03-12 21:24:12 +0000

    Convert Round into Entity.

--- app/src/main/java/uk/theeu/pubgolflad/data/PubGolfRound.kt
+++ app/src/main/java/uk/theeu/pubgolflad/data/PubGolfRound.kt
@@ -1,3 +1,14 @@
 package uk.theeu.pubgolflad.data
 
-data class PubGolfRound(val pub: String, val drink: String, val par: Float, var score: Float?)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class PubGolfRound(
+    @PrimaryKey val uid: Int,
+    @ColumnInfo(name= "pub") val pub: String,
+    @ColumnInfo(name="drink") val drink: String,
+    @ColumnInfo(name="par") val par: Float,
+    @ColumnInfo(name="score") var score: Float?
+)

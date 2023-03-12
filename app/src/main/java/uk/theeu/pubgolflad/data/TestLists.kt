b69121fe84f798b8de456fa9705f0d1784fd171d
George Kokinis <george.kokinis@gmail.com>
2023-03-12 01:06:49 +0000

    Add some mock data.

---
+++ app/src/main/java/uk/theeu/pubgolflad/data/TestLists.kt
@@ -0,0 +1,37 @@
+package uk.theeu.pubgolflad.data
+
+import java.util.*
+
+class TestLists {
+    public fun getGames(): MutableList<PubGolfGame> {
+        return mutableListOf(
+            PubGolfGame(createdAt = Date(),
+                lastUpdated = Date(),
+                title = "Test Game 1",
+                rounds = getRounds()
+            ),
+            PubGolfGame(createdAt = Date(),
+                lastUpdated = Date(),
+                title = "Test Game 2",
+                rounds = getRounds()
+            ),
+        )
+    }
+
+    public fun getRounds(): MutableList<PubGolfRound> {
+        return mutableListOf(
+            PubGolfRound(
+                "Ye Olde Test",
+                "Beeer",
+                2.5f,
+                null,
+            ),
+            PubGolfRound(
+                "Ye Debuggers Delight",
+                "Fireball",
+                1.0f,
+                null,
+            ),
+        )
+    }
+}

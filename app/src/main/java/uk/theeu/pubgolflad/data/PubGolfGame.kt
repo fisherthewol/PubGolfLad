bc8a2596bd6652bc02510fbb6559353e35d5c2ff
George Kokinis <george.kokinis@gmail.com>
2023-03-12 00:52:14 +0000

    Add title to game; viewHolder implementation.

--- app/src/main/java/uk/theeu/pubgolflad/data/PubGolfGame.kt
+++ app/src/main/java/uk/theeu/pubgolflad/data/PubGolfGame.kt
@@ -2,5 +2,5 @@ package uk.theeu.pubgolflad.data
 
 import java.util.*
 
-class PubGolfGame (val createdAt: Date, var lastUpdated: Date, val rounds: MutableList<PubGolfRound> = mutableListOf()) {
+class PubGolfGame (val createdAt: Date, var lastUpdated: Date, val title: String, val rounds: MutableList<PubGolfRound> = mutableListOf()) {
 }
--- app/src/main/java/uk/theeu/pubgolflad/gamesList/GamesAdapter.kt
+++ app/src/main/java/uk/theeu/pubgolflad/gamesList/GamesAdapter.kt
@@ -3,23 +3,26 @@ package uk.theeu.pubgolflad.gamesList
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import uk.theeu.pubgolflad.R
 import uk.theeu.pubgolflad.data.PubGolfGameRepository
 
 class GamesAdapter(private val games: PubGolfGameRepository): RecyclerView.Adapter<GamesAdapter.ViewHolder>() {
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val gametitle: TextView
+        init {
+            gametitle = view.findViewById(R.id.game_title)
+        }
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GamesAdapter.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.games_row_item, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: GamesAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.gametitle.text = games.games.getOrNull(position)?.title ?: "No Title!"
     }
 
     override fun getItemCount(): Int = games.games.size
-
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-
-    }
 }

32364cc60dba9af59bb7137f6828a793cd9a1ef6
George Kokinis <george.kokinis@gmail.com>
2023-03-12 01:12:37 +0000

    Add ID and recycler code to main. Make test functions "static".

--- app/src/main/java/uk/theeu/pubgolflad/MainActivity.kt
+++ app/src/main/java/uk/theeu/pubgolflad/MainActivity.kt
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.appbar.MaterialToolbar
+import uk.theeu.pubgolflad.data.PubGolfGameRepository
+import uk.theeu.pubgolflad.data.getGames
+import uk.theeu.pubgolflad.gamesList.GamesAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        /*Handle top bar*/
         setSupportActionBar(findViewById(R.id.top_app_bar))
         (findViewById<MaterialToolbar>(R.id.top_app_bar)).setOnMenuItemClickListener {
             when (it.itemId) {
@@ -20,6 +25,10 @@ class MainActivity : AppCompatActivity() {
                 else -> {false}
             }
         }
+        /* Instantiate RecyclerView*/
+        val recyclerView: RecyclerView = findViewById(R.id.game_list_recycler)
+        val Repository: PubGolfGameRepository = PubGolfGameRepository(getGames())
+        recyclerView.adapter = GamesAdapter(Repository)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {

c7bcd831ed7a3ad99479350bbef7a73b1c4d351e
George Kokinis <george.kokinis@gmail.com>
2023-03-12 00:38:10 +0000

    Initialise with a Repo; return count.

--- app/src/main/java/uk/theeu/pubgolflad/gamesList/GamesAdapter.kt
+++ app/src/main/java/uk/theeu/pubgolflad/gamesList/GamesAdapter.kt
@@ -5,8 +5,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import uk.theeu.pubgolflad.R
+import uk.theeu.pubgolflad.data.PubGolfGameRepository
 
-class GamesAdapter: RecyclerView.Adapter<GamesAdapter.ViewHolder>() {
+class GamesAdapter(private val games: PubGolfGameRepository): RecyclerView.Adapter<GamesAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GamesAdapter.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.games_row_item, parent, false)
         return ViewHolder(view)
@@ -16,9 +17,7 @@ class GamesAdapter: RecyclerView.Adapter<GamesAdapter.ViewHolder>() {
         TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = games.games.size
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
